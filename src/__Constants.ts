7a1591ed71ef04647345803af6e556cc3445688e
Sheldon Jackson <Sheldon.ej@gmail.com>
2023-06-08 08:41:26 -0400

    First Draft of Landing Page complete
    Is made on screen size: 1682 x 4002
    
    - Currently is not reactivate
    - Hero Image needs to be changed
    - Text needs to be updated

---
+++ src/__Constants.ts
@@ -0,0 +1,13 @@
+/* ------------------------------------- */
+/*         Application Constants         */ 
+/* ------------------------------------- */
+export const APP_NAME  = 'Application Name';
+export const APP_TITLE = 'Application Title';
+//export const APP_DESCRIPTION: string = 'Application Description';
+//export const APP_MAIN_SLOGAN: string = 'Application Keywords';
+//export const APP_SECONDARY_SLOGAN: string = 'Application Keywords';
+//export const APP_TERTIARY_SLOGAN: string = 'Application Keywords';
+
+export const PRODUCTION_PUBLIC_PAGES: Array<string> = [ '/', ];
+export const STAGING_PUBLIC_PAGES: Array<string> = [ '/', ];
+//export const HIDDEN_PAGES: Array<string> = [ ];
---
+++ tailwind.config.ts
@@ -0,0 +1,49 @@
+import { type Config } from "tailwindcss";
+import HERO_BG from '@/images/Desktop_Hero_mage.jpg'
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: { 
+        'poppins': ['Poppins', 'sans-serif'],
+        'work-sans': ['Work Sans', 'sans-serif'], 
+      },
+      fontSize: {
+        'hero-title': '2.75rem',
+      },
+      margin: {
+        'header-gap': '95px',
+      },
+      height: {
+        header: '95px',
+        'main-hero': '625px',
+        'phone': '400px',
+        'section-img': '385px',
+      },
+      borderRadius: {
+        'header-btn': '10px',
+        'primary-btn': '8px',
+        'primary-input': '4px',
+        'section-image': '32px',
+      },
+      backgroundImage: {
+        'hero': 'url(/src/assets/images/Desktop_Hero_Image.jpg)',
+      }
+
+    },
+    colors: {
+      'soundscape-white': '#FFFFFF',
+      'soundscape-blue': '#243A66',
+      'soundscape-bright-blue': '#4DD1FF',
+      'soundscape-dark-blue': '#0F1F38',
+      'soundscape-orange': '#FF5B00',
+      'soundscape-dark-grey': '#4b5364',
+      'soundscape-input-bg': '#FFFFFF1A',
+      'soundscape-blue-bg': '#243A66',
+      'soundscape-grey-bg': '#EDEFF3',
+      'soundscape-white-bg': '#DEE1E5',
+    }
+  },
+  plugins: [],
+} satisfies Config;
