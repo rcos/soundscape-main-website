aa54dc6a3e79d29a492a9a99a284dd3b080d8e79
Sheldon Jackson <Sheldon.ej@gmail.com>
2023-06-08 13:19:17 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
+//delete later
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
