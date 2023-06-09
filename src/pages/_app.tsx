35ca442454171b752c712c038e49a036ccb3e8b3
Sheldon Jackson <Sheldon.ej@gmail.com>
2023-06-09 05:40:05 -0400

    Adding React Hot Toast and Coming Soon Popups

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
+import { Toaster } from 'react-hot-toast';
 //delete later
 //another delete later
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Toaster position="top-center" reverseOrder={false} />
+    <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
