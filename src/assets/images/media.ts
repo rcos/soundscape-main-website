88f93fc1db594116d706d139c61df05a157b0efa
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-06-12 12:30:29 -0400

    usage of exported altImage types

--- src/assets/images/media.ts
+++ src/assets/images/media.ts
@@ -1,7 +1,49 @@
 import { altImage } from "../../utils/type";
-import img1 from "../images/Adidas_Logo.png";
+import ssLogo from "../images/Soundscape_Logo.svg"
+import adidasLogo from "../images/Adidas_Logo.png";
+import heroBG from "../images/HERO.jpg"
+import iphone14First from "../images/iPhone14_First.png"
+import groupIntro from "../images/Group_Intro.jpg"
+import feat1 from "../images/Features_1.png"
+import feat2 from "../images/Features_2.png"
+import feat3 from "../images/Features_3.png"
 
-export const myImage1: altImage = {
-  src: img1,
+export const ssLogoImg: altImage = {
+  src: ssLogo,
+  alt: "",
+};
+
+export const adidasLogoImg: altImage = {
+  src: adidasLogo,
+  alt: "",
+};
+
+export const heroBgImg: altImage = {
+  src: heroBG,
+  alt: "",
+};
+
+export const iphone14Img: altImage = {
+  src: iphone14First,
+  alt: "",
+};
+
+export const groupIntroImg: altImage = {
+  src: groupIntro,
+  alt: "",
+};
+
+export const features1Img: altImage = {
+  src: feat1,
+  alt: "",
+};
+
+export const features2Img: altImage = {
+  src: feat2,
+  alt: "",
+};
+
+export const features3Img: altImage = {
+  src: feat3,
   alt: "",
 };
